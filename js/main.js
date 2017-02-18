2da67107fb751bf2ee2b46f43188ab0c74ef02d5
Murlatron <murley.simon@gmail.com>
2017-02-18 17:30:58 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -65,7 +65,46 @@
 
   	}, 100);
 
+   	 /*----------------------------------------------------*/
+    	/* Sock Counter
+    	------------------------------------------------------ */
+    
+   	 $('#count').waypoint(function() {
 
+    	function animateValue(id, start, end, duration) {
+    	// assumes integer values for start and end
+
+    	var obj = document.getElementById(id);
+    	var range = end - start;
+    	// no timer shorter than 50ms (not really visible any way)
+    	var minTimer = 100;
+    	// calc step time to show all interediate values
+    	var stepTime = Math.abs(Math.floor(duration / range));
+		
+    	// never go below minTimer
+    	stepTime = Math.max(stepTime, minTimer);
+
+    	// get current time and calculate desired end time
+    	var startTime = new Date().getTime();
+    	var endTime = startTime + duration;
+    	var timer;
+
+    	function run() {
+        	var now = new Date().getTime();
+        	var remaining = Math.max((endTime - now) / duration, 0);
+        	var value = Math.round(end - (remaining * range));
+        	obj.innerHTML = value;
+        	if (value == end) {
+        	    clearInterval(timer);
+      	  }
+    	}
+
+    	var timer = setInterval(run, stepTime);
+   	 run();
+	}
+
+	animateValue("count", 0, 100, 5000);
+   	 }, { offset: 'bottom-in-view'});
   	/*----------------------------------------------------*/
   	/* Smooth Scrolling
   	------------------------------------------------------ */
